72ed2ed05f527063a6f5973ce705ce2f6dc8a4da
Motunrayo Ogunyemi <ogunyemiimotunrayo@gmail.com>
2022-02-07 16:22:40 +0100

    update

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,41 +1,24 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import "./card.css"
 
-const Card = () => {
-
-    const [movies, setMovies] = useState([])
-
-    useEffect(() => {
-        fetch('https://adaorachi.github.io/esetech-assessment-api/game-data.json', { method: 'GET',
-        dataType: 'json' }).then(response => {
-            if(response.ok){
-                return response.json()
-            }
-        }).then(data => console.log(data))
-        .then(data => {setMovies(data)})
-    },[])
+const Card = ({movies}) => {
 
     return (
         <div className='e__card'>
-            <div class="e__card-box">
-                <div class="e__card-box-gradient">
-                </div>
-                {movies.map(movie => {
+        {movies.map(movie => {
                     return(
+            <div class="e__card-box">
+                <div class="e__card-box-gradient"></div>
                         <div class="e__card-box-content">
-                            <div>
                             <h2>{movie.name}</h2> 
                             <p>Release date: {movie.first_release_date}</p>
                             <p>{movie.summary}</p>
-                            </div>
                             
                         </div>
-                       
-                        
-                    )
-                })}  
                 <p className="badge badge-warning badge-pill">2</p>
             </div>
+            )
+                })} 
         </div>
     )
 }
--- src/components/filter/Filter.jsx
+++ src/components/filter/Filter.jsx
@@ -1,20 +1,29 @@
 import React from 'react'
 import "./filter.css"
 
-const Filter = () => {
+const Filter = ({searchinput, onFormChange, formSubmit}) => {
+
+    const handleChange = (event) => {
+        onFormChange(event.target.value)
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        formSubmit()
+    }
     return (
         <div className='e__filter'>
             <div className='e__filter-container'>
-                <form className='filter-form'>
+                <form className='filter-form' onSubmit={handleSubmit}>
                     <h2>Filter Results</h2>
-                    <div><label>Name(contains)</label><br/><input type='text' placeholder='text string' className='form-control' /></div>
+                    <div><label>Name(contains)</label><br/><input type='text' value={searchinput} placeholder='text string' className='form-control' onChange={handleChange} /></div>
                     <div><label>Order By</label><br/>
                     <select className='form-control'>
                         <option value=""></option>
                         <option value=""></option>
                     </select>
                     </div>
-                    <div className='text-right'><button className='btn btn-primary mt-2' type='button'>Clear</button></div>
+                    <div className='text-right'><button className='btn btn-primary mt-2'>Clear</button></div>
                 </form>
             </div>
         </div>
